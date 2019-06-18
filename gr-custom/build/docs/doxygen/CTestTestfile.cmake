40e29464352302ede3f65aa7bb773c962078bc35
Rafael Pintar Alevato <rpa.alevato@gmail.com>
2019-06-18 14:24:43 -0300

    Remove build files

--- gr-custom/build/docs/doxygen/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rpa/code/FloripaSat-I-SDR-Receiver/gr-custom/docs/doxygen
-# Build directory: /home/rpa/code/FloripaSat-I-SDR-Receiver/gr-custom/build/docs/doxygen
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
