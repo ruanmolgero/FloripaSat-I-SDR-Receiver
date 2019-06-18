7905a72e5836d4497bfe148c2dafa1410febae4b
Ruan Molgero <ruanmolgero@gmail.com>
2019-06-18 16:42:36 -0300

    Correct FM simulation blocks

--- FM-simulation/fm_simulation.py
+++ FM-simulation/fm_simulation.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Fm Simulation
-# Generated: Tue Jun 18 16:26:16 2019
+# Generated: Tue Jun 18 16:41:20 2019
 ##################################################
 
 from distutils.version import StrictVersion
@@ -28,7 +28,6 @@ from fm_demodulator import fm_demodulator  # grc-generated hier_block
 from fm_modulator import fm_modulator  # grc-generated hier_block
 from gnuradio import analog
 from gnuradio import audio
-from gnuradio import blocks
 from gnuradio import eng_notation
 from gnuradio import filter
 from gnuradio import gr
@@ -37,6 +36,7 @@ from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
 from gnuradio.qtgui import Range, RangeWidget
 from optparse import OptionParser
+from simple_channel import simple_channel  # grc-generated hier_block
 import numpy
 import sip
 from gnuradio import qtgui
@@ -81,14 +81,21 @@ class fm_simulation(gr.top_block, Qt.QWidget):
         self.fs_tx = fs_tx = samp_rate*25/6
         self.gain_FM = gain_FM = 2*pi*kf / fs_tx
         self.fs_SDR = fs_SDR = samp_rate*5
-        self.delta_freq = delta_freq = 1e3
+        self.freq_shift = freq_shift = 1e3
 
         ##################################################
         # Blocks
         ##################################################
-        self._delta_freq_range = Range(0, 200e3, 200, 1e3, 200)
-        self._delta_freq_win = RangeWidget(self._delta_freq_range, self.set_delta_freq, "delta_freq", "counter_slider", float)
-        self.top_layout.addWidget(self._delta_freq_win)
+        self._freq_shift_range = Range(0, 200e3, 200, 1e3, 200)
+        self._freq_shift_win = RangeWidget(self._freq_shift_range, self.set_freq_shift, "freq_shift", "counter_slider", float)
+        self.top_layout.addWidget(self._freq_shift_win)
+        self.simple_channel_0 = simple_channel(
+            freq_shift=freq_shift,
+            phase_shift=0,
+            samp_rate=240e3,
+            noise_amplitude=0,
+            noise_seed=0,
+        )
         self.rational_resampler_xxx_0_0_0_0_1_0 = filter.rational_resampler_fff(
                 interpolation=1,
                 decimation=5,
@@ -155,30 +162,23 @@ class fm_simulation(gr.top_block, Qt.QWidget):
             samp_rate=200e3,
         )
         self.fm_demodulator_0 = fm_demodulator(
-            samp_rate=240e3,
             modulation_sensitivity=30e3,
+            samp_rate=240e3,
         )
-        self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
-        self.blocks_add_xx_0 = blocks.add_vcc(1)
         self.audio_sink_0 = audio.sink(samp_rate, '', True)
-        self.analog_sig_source_x_0_0 = analog.sig_source_c(240e3, analog.GR_COS_WAVE, delta_freq, 1, 0)
         self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, 440, 0.25, 0)
-        self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, 1e-6, 0)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_noise_source_x_0, 0), (self.blocks_add_xx_0, 1))
         self.connect((self.analog_sig_source_x_0, 0), (self.rational_resampler_xxx_0, 0))
-        self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_multiply_xx_0, 1))
-        self.connect((self.blocks_add_xx_0, 0), (self.fm_demodulator_0, 0))
-        self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_add_xx_0, 0))
         self.connect((self.fm_demodulator_0, 0), (self.rational_resampler_xxx_0_0_0_0_1_0, 0))
         self.connect((self.fm_modulator_0, 0), (self.rational_resampler_xxx_0_0_0_0_0, 0))
         self.connect((self.rational_resampler_xxx_0, 0), (self.fm_modulator_0, 0))
-        self.connect((self.rational_resampler_xxx_0_0_0_0_0, 0), (self.blocks_multiply_xx_0, 0))
+        self.connect((self.rational_resampler_xxx_0_0_0_0_0, 0), (self.simple_channel_0, 0))
         self.connect((self.rational_resampler_xxx_0_0_0_0_1_0, 0), (self.audio_sink_0, 0))
         self.connect((self.rational_resampler_xxx_0_0_0_0_1_0, 0), (self.qtgui_freq_sink_x_0_1_0, 0))
+        self.connect((self.simple_channel_0, 0), (self.fm_demodulator_0, 0))
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "fm_simulation")
@@ -228,12 +228,12 @@ class fm_simulation(gr.top_block, Qt.QWidget):
     def set_fs_SDR(self, fs_SDR):
         self.fs_SDR = fs_SDR
 
-    def get_delta_freq(self):
-        return self.delta_freq
+    def get_freq_shift(self):
+        return self.freq_shift
 
-    def set_delta_freq(self, delta_freq):
-        self.delta_freq = delta_freq
-        self.analog_sig_source_x_0_0.set_frequency(self.delta_freq)
+    def set_freq_shift(self, freq_shift):
+        self.freq_shift = freq_shift
+        self.simple_channel_0.set_freq_shift(self.freq_shift)
 
 
 def main(top_block_cls=fm_simulation, options=None):
