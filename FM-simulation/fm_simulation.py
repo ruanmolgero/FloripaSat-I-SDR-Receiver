679cfc9abe81acd265613e52b71e5a69c0a3e2c9
Rafael Pintar Alevato <rpa.alevato@gmail.com>
2019-06-18 16:29:09 -0300

    Add new FM Demodulator Hierarchical Block
    Also Change FM Modulator category.

--- FM-simulation/fm_simulation.py
+++ FM-simulation/fm_simulation.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Fm Simulation
-# Generated: Tue Jun 18 15:29:41 2019
+# Generated: Tue Jun 18 16:26:16 2019
 ##################################################
 
 from distutils.version import StrictVersion
@@ -24,6 +24,7 @@ sys.path.append(os.environ.get('GRC_HIER_PATH', os.path.expanduser('~/.grc_gnura
 
 from PyQt5 import Qt
 from PyQt5 import Qt, QtCore
+from fm_demodulator import fm_demodulator  # grc-generated hier_block
 from fm_modulator import fm_modulator  # grc-generated hier_block
 from gnuradio import analog
 from gnuradio import audio
@@ -153,13 +154,11 @@ class fm_simulation(gr.top_block, Qt.QWidget):
             modulation_sensitivity=30e3,
             samp_rate=200e3,
         )
-        self.dc_blocker_xx_0 = filter.dc_blocker_ff(2048, True)
-        self.blocks_multiply_xx_1 = blocks.multiply_vcc(1)
+        self.fm_demodulator_0 = fm_demodulator(
+            samp_rate=240e3,
+            modulation_sensitivity=30e3,
+        )
         self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
-        self.blocks_multiply_const_vxx_0_0 = blocks.multiply_const_vff((fs_SDR/(2*pi*kf), ))
-        self.blocks_delay_1 = blocks.delay(gr.sizeof_gr_complex*1, 1)
-        self.blocks_conjugate_cc_0 = blocks.conjugate_cc()
-        self.blocks_complex_to_arg_0 = blocks.complex_to_arg(1)
         self.blocks_add_xx_0 = blocks.add_vcc(1)
         self.audio_sink_0 = audio.sink(samp_rate, '', True)
         self.analog_sig_source_x_0_0 = analog.sig_source_c(240e3, analog.GR_COS_WAVE, delta_freq, 1, 0)
@@ -172,15 +171,9 @@ class fm_simulation(gr.top_block, Qt.QWidget):
         self.connect((self.analog_noise_source_x_0, 0), (self.blocks_add_xx_0, 1))
         self.connect((self.analog_sig_source_x_0, 0), (self.rational_resampler_xxx_0, 0))
         self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_multiply_xx_0, 1))
-        self.connect((self.blocks_add_xx_0, 0), (self.blocks_delay_1, 0))
-        self.connect((self.blocks_add_xx_0, 0), (self.blocks_multiply_xx_1, 0))
-        self.connect((self.blocks_complex_to_arg_0, 0), (self.blocks_multiply_const_vxx_0_0, 0))
-        self.connect((self.blocks_conjugate_cc_0, 0), (self.blocks_multiply_xx_1, 1))
-        self.connect((self.blocks_delay_1, 0), (self.blocks_conjugate_cc_0, 0))
-        self.connect((self.blocks_multiply_const_vxx_0_0, 0), (self.dc_blocker_xx_0, 0))
+        self.connect((self.blocks_add_xx_0, 0), (self.fm_demodulator_0, 0))
         self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_add_xx_0, 0))
-        self.connect((self.blocks_multiply_xx_1, 0), (self.blocks_complex_to_arg_0, 0))
-        self.connect((self.dc_blocker_xx_0, 0), (self.rational_resampler_xxx_0_0_0_0_1_0, 0))
+        self.connect((self.fm_demodulator_0, 0), (self.rational_resampler_xxx_0_0_0_0_1_0, 0))
         self.connect((self.fm_modulator_0, 0), (self.rational_resampler_xxx_0_0_0_0_0, 0))
         self.connect((self.rational_resampler_xxx_0, 0), (self.fm_modulator_0, 0))
         self.connect((self.rational_resampler_xxx_0_0_0_0_0, 0), (self.blocks_multiply_xx_0, 0))
@@ -197,9 +190,9 @@ class fm_simulation(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.set_fs_SDR(self.samp_rate*5)
         self.qtgui_freq_sink_x_0_1_0.set_frequency_range(0, self.samp_rate)
         self.set_fs_tx(self.samp_rate*25/6)
+        self.set_fs_SDR(self.samp_rate*5)
         self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
 
     def get_pi(self):
@@ -208,7 +201,6 @@ class fm_simulation(gr.top_block, Qt.QWidget):
     def set_pi(self, pi):
         self.pi = pi
         self.set_gain_FM(2*self.pi*self.kf / self.fs_tx)
-        self.blocks_multiply_const_vxx_0_0.set_k((self.fs_SDR/(2*self.pi*self.kf), ))
 
     def get_kf(self):
         return self.kf
@@ -216,7 +208,6 @@ class fm_simulation(gr.top_block, Qt.QWidget):
     def set_kf(self, kf):
         self.kf = kf
         self.set_gain_FM(2*self.pi*self.kf / self.fs_tx)
-        self.blocks_multiply_const_vxx_0_0.set_k((self.fs_SDR/(2*self.pi*self.kf), ))
 
     def get_fs_tx(self):
         return self.fs_tx
@@ -236,7 +227,6 @@ class fm_simulation(gr.top_block, Qt.QWidget):
 
     def set_fs_SDR(self, fs_SDR):
         self.fs_SDR = fs_SDR
-        self.blocks_multiply_const_vxx_0_0.set_k((self.fs_SDR/(2*self.pi*self.kf), ))
 
     def get_delta_freq(self):
         return self.delta_freq
