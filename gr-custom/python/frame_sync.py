ed24ccf09aecf4de030b41dfdd51e283e6f2a4e8
Ruan Molgero <ruanmolgero@gmail.com>
2019-06-24 13:02:11 -0300

    Finish logic of frame-sync block
    Save received message as binary in a file.

--- gr-custom/python/frame_sync.py
+++ gr-custom/python/frame_sync.py
@@ -22,12 +22,13 @@
 import numpy
 import bitarray
 from gnuradio import gr
+from struct import pack
 
 class frame_sync(gr.sync_block):
     """
     docstring for block frame_sync
     """
-    def __init__(self, sync_word, threshold):
+    def __init__(self, sync_word, threshold, bytes_message_size):
         gr.sync_block.__init__(self,
             name="frame_sync",
             in_sig=[numpy.float32],
@@ -38,58 +39,60 @@ class frame_sync(gr.sync_block):
         #sync_word = [1,0,1,0,1,0,1,0,1,0,1,0]
         self.sync_word = sync_word
         self.threshold = threshold
+        self.bits_message_size = 8*bytes_message_size
         self.sync_word_size = len(self.sync_word)
-        self.last_samples = []
         self.delay = 0
         self.correlation = 0
-        self.detected = False
         self.multiplication_buffer = []
-        self.message_length = []
-        self.message = []
+        self.message_length_string = ""
+        self.message_length_int = 0
+        self.message = ""
+
+        # Clean old file
+        self.file = open("message.bin", "wb")
+        # Open file for writing
+        self.file = open("message.bin", "ab")
 
     def work(self, input_items, output_items):
         in0 = input_items[0]
         out = output_items[0]
 
-        # for i in range(len(in0)):
-        #     # Delay for filling sample_buffer
-        #     if self.delay < self.sync_word_size:
-        #         self.sample_buffer.append(in0[i])
-        #         out[i] = 0
-        #         self.delay += 1
-        #     else:
-        #         self.sample_buffer.append(in0[i])
-        #         self.sample_buffer.pop(0)
-
         for i in range(len(in0)):
             # Delay for filling the buffers
             if self.delay < (self.sync_word_size-1):
                 self.multiplication_buffer.append(in0[i]*sync_word[i])
+                self.correlation += multiplication_buffer[-1]
                 out[i] = 0
                 self.delay += 1
             else:
                 self.multiplication_buffer.append(in0[i]*sync_word[i])
+                self.correlation += multiplication_buffer[-1]
 
                 if (!detected):
-                    self.correlation = sum(multiplication_buffer)
+                    out[i] = 0
                     if self.correlation >= self.threshold:
                         detected = True
                 else:
+                    out[i] = in0[i]
                     if in0[i] < 0:
                         in0[i] = 0
 
-                    if(len(message_length) < 24):
-                        self.message_length.append(in0[i])
+                    if(len(message_length_string) < bits_message_size):
+                        self.message_length_string += str(in0[i])
+                        if (len(message_length_string) == bits_message_size):
+                            message_length_int = int(message_length_string, 2)
                     else:
-                        message_length_int = int("".join(map(str, message_length)),2)
-
                         if(len(message) < message_length_int):
-                            self.message.append(in0[i])
+                            self.message += str(in0[i])
                         else:
-                            for x in range(len(message)):
-                                message_str = ''.join(str(message) for bit in message)
-                             
-
+                            number_bytes = len(self.messsage) % 8
+                            for i in range(number_bytes):
+                                self.file.write(pack('i', int(self.message[i*8: i*8+8], 2)))
+                            self.file.write(pack('i', int(self.message[number_bytes:], 2)))
+                            self.detected = False
+                            self.message = ""
+                            self.message_length_string = ""
+                            self.message_length_int = 0
 
                 self.correlation += (-1)*multiplication_buffer[0]
                 self.multiplication_buffer.pop(0)
